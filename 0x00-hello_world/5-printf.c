3e4cb734be2a51c086812346b8cabeccafa0621d
Sarah <sarahossai6@gmail.com>
2022-06-30 08:15:54 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: retuen 0 everytime
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n"
+        return (0);
+}
