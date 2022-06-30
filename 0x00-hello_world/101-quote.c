2b94055cb43726744e526018058248f0c81580e2
Sarah <sarahossai6@gmail.com>
2022-06-30 09:13:23 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main
+ *main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a new line, to standard error.
  *
  *Return: return 1
  */
